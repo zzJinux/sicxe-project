ba98a5977a81b36243a1c7cd14aa6e26ba9539f2
Jinwook Jeong <vustthat@gmail.com>
2018-04-06 02:26:14 +0900

    *주석* assemble_consts.h, assemble_errs.h

--- assemble_consts.h
+++ assemble_consts.h
@@ -1,14 +1,21 @@
+/** assemble_consts.h
+ *  assemble에 사용되는 상수들을 정의
+ */
 #ifndef _ASSEMBLE_CONSTS_H_
 #define _ASSEMBLE_CONSTS_H_
 
 #define PROGRAM_NAME_LIMIT 6
 #define LOC_MAX 0xffff
+
+// WORD 관련 상수들
 #define WORD_SIZE 3
 #define WORD_MAX ((1<<(WORD_SIZE*8-1))-1)
 #define WORD_MIN (-(1<<(WORD_SIZE*8-1)))
 
+// .obj 코드의 text record의 최대 길이(byte)
 #define TRECORD_CODE_LIMIT 30
 
+// 각 directive의 이름
 #define DIRECTIVE_START "START"
 #define DIRECTIVE_END "END"
 #define DIRECITVE_BYTE "BYTE"
@@ -18,6 +25,7 @@
 #define DIRECTIVE_BASE "BASE"
 #define DIRECTIVE_NOBASE "NOBASE"
 
+// pass2 에서 사용되는 flag 상수
 enum _AssembleStateFlag {
   NO_OBJECT_CODE = 1<<0,
   BASE_AVAILABLE = 1<<1,
--- assemble_errs.h
+++ assemble_errs.h
@@ -1,25 +1,28 @@
+/** assemble_errs.h
+ *  assemble 중 발생가능한 에러들에 대응되는 상수들을 정의
+ */
 #ifndef _ASSEMBLE_ERRS_H_
 #define _ASSEMBLE_ERRS_H_
 
 typedef enum _ASSEMBLE_ERROR {
-  ADDR_RESOLUTION_FAIL = 1<<3,
-  SYNTAX_PARSE_FAIL = 1<<4,
-  ALLOC_FAIL = 1<<5
+  ADDR_RESOLUTION_FAIL = 1<<3, // 주소를 표현할 방법이 없는 경우
+  SYNTAX_PARSE_FAIL = 1<<4, // 문법오류, 세부 내용은 _SYNTAX_ERROR 상수를 이용
+  ALLOC_FAIL = 1<<5 // 동적할당 오류
 } ASSEMBLE_ERROR;
 
 typedef enum _SYNTAX_ERROR {
-  START_DIRECTIVE_WRONG_PLACE = 1,
-  PROGRAM_NAME_TOO_LONG,
-  LOC_EXCEEDED,
-  INVALID_OPERAND_FORMAT,
-  INVALID_SYMBOL_NAME,
-  OPERAND_MISSING,
-  DUPLICATE_SYMBOL,
-  UNDEFINED_OPCODE,
-  UNDEFINED_SYMBOL,
-  CONSTANT_TOO_LARGE,
-  TOKEN_MISSING,
-  UNDEFINED_REGISTER
+  START_DIRECTIVE_WRONG_PLACE = 1, // START 지시자가 첫 줄이 아닌 곳에 있음
+  PROGRAM_NAME_TOO_LONG, // 프로그램 이름 길이가 너무 김
+  LOC_EXCEEDED, // location counter가 허용범위를 넘어섬
+  INVALID_OPERAND_FORMAT, // 잘못된 operand 형식
+  INVALID_SYMBOL_NAME, // 잘못된 symbol 형식
+  OPERAND_MISSING, // operand가 필요한데 없음
+  DUPLICATE_SYMBOL, // 같은 symbol이 두 개 이상의 위치에서 정의됨
+  UNDEFINED_OPCODE, // opcode table에 없는 mnemonic이 사용됨
+  UNDEFINED_SYMBOL, // symbol table에 없는 symbol이 사용됨
+  CONSTANT_TOO_LARGE, // 상수가 너무 큼
+  TOKEN_MISSING, // oken이 필요한데 없음
+  UNDEFINED_REGISTER // 정의에 없는 register 번호
 } SYNTAX_ERROR;
 
 char const *SYNTAX_ERROR_MESSAGES[] = {
